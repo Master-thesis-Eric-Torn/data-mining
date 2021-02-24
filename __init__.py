3046b88fd43ab92f8b5240c702269940f5881209
Eric Thörn <eric.m.thorn@gmail.com>
2021-02-24 14:28:54 +0100

    mega commit: implemented a data_miner class that can be run on any machine with python.

--- __init__.py
+++ __init__.py
@@ -1,103 +1,101 @@
 from data_mining.data_sources import TibberAPI, SensiboSensor, WeatherAPI
 import zipfile
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 from pathlib import Path
+import logging
+import atexit
 import asyncio
+import nest_asyncio
+nest_asyncio.apply()
 
-'''
-Data Mining module
-==================
-'''
 LOCAL_TIMEZONE = pytz.timezone('Europe/Oslo')
 DEFAULT = {
     "sampling_time": 60*60,
+    "end_mining_at": None,
+    "end_backup_at": None,
+    "max_backup_copies": 5
 }
-
-# params = {
-#     'tibber': {
-#         'api_key': 'dx61b7ne2HzGbpt0gI0xrB1f0jxd0_heXs-dmoMcnuA',
-#         'sampling_time': 60*60,
-#         'end_mining_at': None
-#     },
-#     'sensibo': {
-#         'api_key': 'tXZ5Ibk5s95UZgZgOU3wzJ4MOunl2f',
-#         'sampling_time': 5*60,
-#         'end_mining_at': None
-#     },
-#     'weather': {
-#         'api_key': 'c512438f8c9099db879c8fec88b10111',
-#         'lat': "63.4",
-#         'lon': "10.335",
-#         'sampling_time': 60*60,
-#         'end_mining_at': None
-#     },
-#     'backup': {
-#         'run_backups': True,
-#         'end_backups_at': None
-#     }
-# }
-
-# ########
-# # USAGE
-# dataMiner = DataMiner(params)
-# dataMiner.start()
-# toThread(dataMiner.start_backups())
-
-# # For raspberry:
-# dataMiner = DataMiner(params)
-# toThread(dataMiner.start())
-# toThread(dataMiner.start_backups())
-
-
-# TODO:
-# * Add he initial sensor parameters below to the __init__ in sensors
 
 
 class DataMiner():
-    def __init__(self, params):
+    '''
+    Data Mining module
+    ==================
+    Saves data from:
+    * TibberAPI
+    * Sensibo
+    * Open Weather Map API
+
+    Stores in files as pickle format.
+
+    Backups are by default ran once every day at midnight, as well as whenever the program exits.
+    '''
+    def __init__(self, params={}):
+        logging.basicConfig(filename="datamining_errors.log",
+                            format='%(asctime)s %(message)s', level=logging.DEBUG)
+        atexit.register(self._backup, object_to_backup='data', backup_folder='backups')
         self._mining_coroutines = []
         # Initialize all sensors which has been provided an API key
-        if params['tibber']['api_key']:
-            sampling_time = params['tibber']['sampling_time']
+        try:
+            tibber = params['tibber']
+            sampling_time = tibber.get('sampling_time', DEFAULT["sampling_time"])
+            end_mining_at = tibber.get('end_mining_at', DEFAULT["end_mining_at"])
             self.tibberAPI = TibberAPI(
-                    api_key = params['tibber']['api_key'], 
-                    sampling_time = sampling_time if sampling_time else DEFAULT["sampling_time"],
+                    api_key = tibber['api_key'],
+                    sampling_time = sampling_time,
                     end_mining_at = params['tibber']['end_mining_at']
                 )
             self._mining_coroutines.append(self.tibberAPI.start_mining())
+        except:
+            print("Tibber api_key not found in parameters. Will not include tibber in data mining.")
 
-        if params['sensibo']['api_key']:
-            sampling_time = params['sensibo']['sampling_time']
+        try:
+            sensibo = params['sensibo']
+            sampling_time = sensibo.get('sampling_time', DEFAULT["sampling_time"])
+            end_mining_at = sensibo.get('end_mining_at', DEFAULT["end_mining_at"])
             self.sensiboSensor = SensiboSensor(
-                    api_key = params['sensibo']['api_key'],
-                    sampling_time = sampling_time if sampling_time else DEFAULT["sampling_time"],
-                    end_mining_at = params['sensibo']['end_mining_at']
+                    api_key = sensibo['api_key'],
+                    sampling_time = sampling_time,
+                    end_mining_at = end_mining_at
                 )
             self._mining_coroutines.append(self.sensiboSensor.start_mining())
+        except:
+            print("Sensibo api_key not found in parameters. Will not include sensibo in data mining.")
 
-        if params['weather']['api_key']:
-            sampling_time = params['weather']['sampling_time']
+        try:
+            weather = params['weather']
+            sampling_time = weather.get('sampling_time', DEFAULT["sampling_time"])
+            end_mining_at = weather.get('end_mining_at', DEFAULT["end_mining_at"])
             self.weatherAPI = WeatherAPI(
                     api_key = params['weather']['api_key'], 
-                    sampling_time = sampling_time if sampling_time else DEFAULT["sampling_time"],
-                    end_mining_at = params['weather']['end_mining_at'],
+                    sampling_time = sampling_time,
+                    end_mining_at = end_mining_at,
                     lat = params['weather']['lat'],
                     lon = params['weather']['lon']
                 )
             self._mining_coroutines.append(self.weatherAPI.start_mining())
+        except:
+            print("Weather api_key, latiture or longitude not found in parameters. Will not include sensibo in data mining.")
 
         # Backups parameters
-        if params['backup']['run_backups'] == True:
+        self.end_backup_time = DEFAULT["end_backup_at"]
+        try:
+            backup_params = params['backup']
+            self.run_backups = backup_params.get('run_backups', False)
+            self.end_backup_time = backup_params.get('end_backups_at', DEFAULT["end_backup_at"])
+        except:
             self.run_backups = True
-            self.end_backup_time = params['backup']['end_backups_at']
+            print("Backup parameters not found, running with default backup params: run_backups=True, end_backup_at=None")
+        
+        print("-----------------")
+
 
     def start(self):
         asyncio.run(self._async_start())
         return
 
     async def _async_start(self):
-
         await asyncio.gather(
             *self._mining_coroutines,
             self._async_backup_data()
@@ -110,18 +108,31 @@ class DataMiner():
             return
 
         scheduled_time = self._get_midnight_time()
+        end_backup_time = self.end_backup_time if self.end_backup_time else "Never"
+        print(f"Backups started | next backup: {scheduled_time} | ending: {end_backup_time}")
+        
         while self.end_backup_time is None or scheduled_time < self.end_backup_time:
             time_now = datetime.now(tz=LOCAL_TIMEZONE)
             time_until_backup = (scheduled_time - time_now).total_seconds()
             if time_until_backup > 0:
                 await asyncio.sleep(time_until_backup)
-            self._backup('data', 'backups')
-            scheduled_time += timedelta(seconds=self.sampling_time)
+            
+            backup_success = self._backup('data', 'backups')
+            success_message = ""
+            if backup_success:
+                success_message += f"BACKUP | {scheduled_time} | Success: backuped files in /backups/"
+            
+            scheduled_time += timedelta(hours=24)
+            
+            if success_message:
+                message = f"{success_message} | Next backup at: {scheduled_time}"
+                logging.info(message)
+                print(message)
         return
 
     def _backup(self, object_to_backup, backup_folder):
         # settings
-        max_backups_amount = 5
+        max_backup_copies = DEFAULT["max_backup_copies"]
 
         # Setup paths and folder
         object_to_backup_path = Path(object_to_backup)
@@ -134,17 +145,17 @@ class DataMiner():
         # check how many existing backups exist
         existing_backups = [
             x for x in backup_directory_path.iterdir()
-            if x.is_file() and x.suffix == '.zip' and x.name.startswith('backup-')
+            if x.is_file() and x.suffix == '.zip' and x.name.startswith('data_backup')
         ]
 
         # Enforce max backups and delete oldest if there will be too many after the new backup
         oldest_to_newest_backup_by_name = list(sorted(existing_backups, key=lambda f: f.name))
-        while len(oldest_to_newest_backup_by_name) >= max_backups_amount:  # >= because we will have another soon
+        while len(oldest_to_newest_backup_by_name) >= max_backup_copies:  # >= because we will have another soon
             backup_to_delete = oldest_to_newest_backup_by_name.pop(0)
             backup_to_delete.unlink()
 
         # Create zip file (for both file and folder options)
-        backup_file_name = f'backup-{datetime.now().strftime("%Y%m%d%H%M%S")}-{object_to_backup_path.name}.zip'
+        backup_file_name = f'{object_to_backup_path.name}_backup__{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.zip'
         zip_file = zipfile.ZipFile(str(backup_directory_path / backup_file_name), mode='w')
         if object_to_backup_path.is_file():
             # If the object to write is a file, write the file
@@ -169,25 +180,3 @@ class DataMiner():
         time_now = datetime.now(tz=LOCAL_TIMEZONE)
         midnight = time_now.replace(microsecond=0, second=0, minute=0, hour=0, day=time_now.day+1)
         return midnight
-
-    def set_settings(self):
-        # TODO: return all settings/parameters
-        # self.sensiboSensor.some_setting = something
-        # self.sensiboSensor.some_other_setting = somethingElse
-        return
-    
-    def get_settings(self):
-        # TODO: not a priority
-        settings = {}
-        return settings
-
-    async def _async_store_data_together(self):
-        # TODO
-        # put together at every hour?
-
-        # Timeschedule
-        # while
-        #   backup
-        #   timeschedule
-        #   asyncio sleep
-        pass
--- data_sources/__init__.py
+++ data_sources/__init__.py
@@ -27,73 +27,81 @@ class Sensor():
     def __init__(self):
         logging.basicConfig(filename="datamining_errors.log",
                             format='%(asctime)s %(message)s', level=logging.DEBUG)
-        self._session_id = str(uuid1())[:8]
+        self._session_id = str(int(time.time())) + '-' + str(uuid1())[:8]
         self._initialize_data_folders()
 
-    async def start_mining(self, time_ending=None):
+    async def start_mining(self):
         scheduled_time = self._get_first_scheduled_time()
         self.time_mining_started = scheduled_time
-        self.time_ending = time_ending
-
-        while self.time_ending is None or scheduled_time < self.time_ending:
+        end_mining_at = self.end_mining_at if self.end_mining_at else "Never"
+        print(f"{self.sensor_name} started mining | next scheduled measurement: {scheduled_time} | ending: {end_mining_at}")
+        while self.end_mining_at is None or scheduled_time < self.end_mining_at:
             time_now = datetime.now(tz=LOCAL_TIMEZONE)
             time_until_measurement = (
                 scheduled_time - time_now).total_seconds()
             if time_until_measurement > 0:
                 await asyncio.sleep(time_until_measurement)
 
-            # _get_latest_measurement() is unique for each sensor and is defined in each sensor file
-            await self._get_latest_measurement()
-            self._save_data_to_file()
+            # _get_latest_measurement() is unique for each sensor and is defined in each sensor class below
+            response = await self._get_latest_measurement()
+            success_message = ""
+            if response:
+                saved = self._save_data_to_file()
+                if saved:
+                    success_message += f"{self.sensor_name} | {scheduled_time} | Success: got measurement and stored to file"
 
             # set the time for next measurement
             scheduled_time += timedelta(seconds=self.sampling_time)
+            if success_message:
+                message = f"{success_message} | Next scheduled time: {scheduled_time}"
+                logging.info(message)
+                print(message)
 
     def _save_data_to_file(self):
         time_now = datetime.now(tz=LOCAL_TIMEZONE)
-        
-        folder_daily = self.sensor_name + '/daily/'
+        basedir = os.getcwd()+os.sep+'data'+os.sep
+
+        folder_daily = basedir+self.sensor_name+os.sep+'daily'+os.sep
         filename_daily = self.sensor_name + '_' + \
-            time_now.strftime('%Y-%m-%d') + '_' + self._session_id
+            time_now.strftime('%Y-%m-%d') + '__' + self._session_id
 
-        folder_weekly = self.sensor_name + '/weekly/'
+        folder_weekly = basedir+self.sensor_name+os.sep+'weekly'+os.sep
         filename_weekly = self.sensor_name + '_' + \
-            time_now.strftime('%Y-week-%V') + '_' + self._session_id
+            time_now.strftime('%Y-week-%V') + '__' + self._session_id
 
-        folder_monthly = self.sensor_name + '/monthly/'
+        folder_monthly = basedir+self.sensor_name+os.sep+'monthly'+os.sep
         filename_monthly = self.sensor_name + '_' + \
-            time_now.strftime('%Y-month-%m') + '_' + self._session_id
+            time_now.strftime('%Y-month-%m') + '__' + self._session_id
 
-        folder_yearly = self.sensor_name + '/yearly/'
+        folder_yearly = basedir+self.sensor_name+os.sep+'yearly'+os.sep
         filename_yearly = self.sensor_name + '_' + \
-            time_now.strftime('%Y') + '_' + self._session_id
+            time_now.strftime('%Y') + '__' + self._session_id
 
         try:
-            with open(folder_daily+filename_daily, "wb") as f:
+            with open(folder_daily+filename_daily+".pkl", "wb") as f:
                 pickle.dump(self.data, f)
-            return
-            with open(folder_weekly+filename_weekly, "wb") as f:
+            with open(folder_weekly+filename_weekly+".pkl", "wb") as f:
                 pickle.dump(self.data, f)
-            return
-            with open(folder_monthly+filename_monthly, "wb") as f:
+            with open(folder_monthly+filename_monthly+".pkl", "wb") as f:
                 pickle.dump(self.data, f)
-            return
-            with open(folder_yearly+filename_yearly, "wb") as f:
+            with open(folder_yearly+filename_yearly+".pkl", "wb") as f:
                 pickle.dump(self.data, f)
-            return
         except Exception as e:
             logging.error(
                 "save_data_to_file: Something went wrong while writing data to files.")
             logging.error(str(e))
+            print(f"{self.sensor_name}: save_data_to_file: Something went wrong while writing data to files.")
+            print(str(e))
             return False
         return True
 
     def _get_first_scheduled_time(self):
         time_now = datetime.now(tz=LOCAL_TIMEZONE)
-        dt = self.sampling_time
-        if int(dt*np.ceil(time_now.minute/dt)) < 60:
+        # seconds to minutes
+        dt_minutes = self.sampling_time/60
+        if int(dt_minutes*np.ceil(time_now.minute/dt_minutes)) < 60:
             scheduled_time = time_now.replace(
-                second=0, microsecond=0, minute=int(dt*np.ceil(time_now.minute/dt)))
+                second=0, microsecond=0, minute=int(dt_minutes*np.ceil(time_now.minute/dt_minutes)))
         else:
             scheduled_time = time_now.replace(
                 second=0, microsecond=0, minute=0, hour=time_now.hour+1)
@@ -101,10 +109,10 @@ class Sensor():
 
     def _initialize_data_folders(self):
         try:
-            os.makedirs('data/'+self.sensor_name+'/daily')
-            os.makedirs('data/'+self.sensor_name+'/weekly')
-            os.makedirs('data/'+self.sensor_name+'/monthly')
-            os.makedirs('data/'+self.sensor_name+'/yearly')
+            os.makedirs('data/'+self.sensor_name+'/daily', exist_ok=True)
+            os.makedirs('data/'+self.sensor_name+'/weekly', exist_ok=True)
+            os.makedirs('data/'+self.sensor_name+'/monthly', exist_ok=True)
+            os.makedirs('data/'+self.sensor_name+'/yearly', exist_ok=True)
         except Exception as e:
             logging.error("ERROR: " + str(e))
             print("ERROR (_initialize_data_folders): " + str(e))
@@ -132,13 +140,11 @@ class TibberAPI(Sensor):
         - n: > 0
             = how many datapoints to pull
     '''
-    def __init__(self, api_key, sampling_time, end_mining_at):
+    def __init__(self, api_key, sampling_time, end_mining_at=None):
         self.sensor_name = 'tibber'
         self.api_key = api_key
         self.sampling_time = sampling_time
         self.end_mining_at = end_mining_at
-        super().__init__()
-
         try:
             self._tibber_conn = tibber.Tibber(self.api_key)
             self._tibber_conn.sync_update_info()
@@ -146,24 +152,22 @@ class TibberAPI(Sensor):
         except Exception as e:
             print(str(e))
             print("Tibber: could not connect.")
-        
-        
         self._initialize_data_structure()
+        super().__init__()
 
-    async def get_latest_measurement(self):
+    async def _get_latest_measurement(self):
         for home in self.homes:
             try:
-                await home.sync_update_info()
-            except:
-                # TODO: Logger
+                home.sync_update_info()
+            except Exception as e:
+                logging.error("Tibber: could not sync home info: " + str(e))
                 print("Tibber: could not sync home info")
+                print(str(e))
                 return False
-            
             # Settings
             home_name = home.info['viewer']['home']['appNickname']
             resolution = "HOURLY"
             n = 1
-
             # Initialize data structure for each home
             self.data['data'][home_name] = {
                 'time': [],
@@ -171,19 +175,19 @@ class TibberAPI(Sensor):
                 'cost': [],
                 'total_cost': []
             }
-
             # Get data
             try:
-                historic_data = await home.sync_get_historic_data(n, resolution)
+                historic_data = home.sync_get_historic_data(n, resolution)
                 for data_point in historic_data:
                     timestamp = datetime.strptime(data_point['from'], '%Y-%m-%dT%H:%M:%S%z')
-                    self.data[home_name]['time'].append(timestamp)
-                    self.data[home_name]['consumption'].append(data_point['consumption'])
-                    self.data[home_name]['cost'].append(data_point['cost'])
-                    self.data[home_name]['total_cost'].append(data_point['totalCost'])
-            except:
-                # TODO: Logger
+                    self.data['data'][home_name]['time'].append(timestamp)
+                    self.data['data'][home_name]['consumption'].append(data_point['consumption'])
+                    self.data['data'][home_name]['cost'].append(data_point['cost'])
+                    self.data['data'][home_name]['total_cost'].append(data_point['totalCost'])
+            except Exception as e:
+                logging.error("Tibber: could not get historic data. " + str(e))
                 print("Tibber: could not get historic data.")
+                print(str(e))
                 return False
         return True
 
@@ -211,26 +215,24 @@ class SensiboSensor(Sensor):
         self.what_to_measure = ['temperature', 'humidity']
         self.states_to_record = ['on', 'targetTemperature', 'fanLevel', 'mode']
         self.time_since_last_measurement = None
-        super().__init__()
-
         try:
             self.home = SC.SensiboClientAPI(self.api_key)
             self.devices = self.home.devices()
         except:
             print('Home Sensibo could not be accessed. Code terminated.')
             sys.exit()
-
-        
         self._initialize_data_structure()
+        super().__init__()
 
     async def _get_latest_measurement(self):
         for pump in self.devices.keys():
             try:
-                latest_measurement = await self.home.pod_measurement(self.devices[pump])[0]
-                current_state = await self.home.pod_ac_state(self.devices[pump])
-            except:
-                # TODO: logger
+                latest_measurement = self.home.pod_measurement(self.devices[pump])[0]
+                current_state = self.home.pod_ac_state(self.devices[pump])
+            except Exception as e:
+                logging.error("Sensibo get_latest_measurement: Could not get measurements or state" + str(e))
                 print("Sensibo get_latest_measurement: Could not get measurements or state")
+                print(str(e))
                 return False
             
             # TODO: Setting: "avoid duplicates"
@@ -243,7 +245,6 @@ class SensiboSensor(Sensor):
                     self.data['data'][pump]['measurements'][measurement_type].append(latest_measurement[measurement_type])
                 else:
                     self.data['data'][pump]['measurements'][measurement_type].append(np.NaN)
-                    # TODO: Logger
                     print(latest_measurement)
                     print('Sensibo measurement '+str(measurement_type) +' for '+str(pump)+' missing, put NaN ')
 
@@ -252,12 +253,11 @@ class SensiboSensor(Sensor):
                         self.data['data'][pump]['states'][state].append(current_state[state])
                     else:
                         self.data['data'][pump]['states'][state].append(np.NaN)
-                        # TODO: Logger
                         print(current_state)
                         print('Sensibo state '+str(state)+' for ' + str(pump)+' missing, put NaN ')
             timestamp = datetime.strptime(latest_measurement['time']['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
             utc_timestamp = timestamp.replace(tzinfo=pytz.utc)
-            data[pump]['times'].append(utc_timestamp.astimezone(tz=LOCAL_TIMEZONE))
+            self.data['data'][pump]['times'].append(utc_timestamp.astimezone(tz=LOCAL_TIMEZONE))
             self.time_since_last_measurement = latest_measurement['time']['secondsAgo']
         return True
 
@@ -296,13 +296,13 @@ class WeatherAPI(Sensor):
         self.end_mining_at = end_mining_at
         self.lat = lat
         self.lon = lon
-        self.url = f"api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={self.api_key}&units=metric"
-        super().__init__()
+        self.url = f"http://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={self.api_key}&units=metric"
         self._initialize_data_structure()
+        super().__init__()
 
     async def _get_latest_measurement(self):
         try:
-            response = await requests.get(self.url, timeout=30)
+            response = requests.get(self.url, timeout=30)
             if response.status_code == 200:
                 current_data = json.loads(response.text)
                 timestamp = datetime.utcfromtimestamp(current_data['dt']).replace(tzinfo=timezone.utc)
@@ -315,13 +315,14 @@ class WeatherAPI(Sensor):
                 self.current["temperature"] = current_data['main']['temp']
                 self.last_updated = response.headers["Date"]
             else: 
-                # TODO: logger
-                print("Weather API: Could not get request from the URL. Check API key, lat, or lon.")
-                print("Request status code: ", response.status_code)
+                logging.error("Weather API: Could not get request from the URL. Check API key, lat, or lon: " + str(response.status_code))
+                print("Weather API: Could not get request from the URL. Check API key, lat, or lon:")
+                print("===> Request status code: ", response.status_code)
                 return False
-        except:
-            # TODO: logger
-            print("Weather API: Could not get request from the URL. Check API key, lat, or lon.")
+        except Exception as e:
+            logging.error("Weather API: Could not get request from the URL. Check API key, lat, or lon: " + str(e))
+            print("Weather API: Could not get request from the URL. Check API key, lat, or lon:")
+            print('===> ' + str(e))
             return False
         return True
 
@@ -340,6 +341,7 @@ class SpotMarketAPI(Sensor):
     '''
     Spot market API
     ===============
+    TODO?
     '''
     def __init__(self, zone='Tr.heim'):
         self.spot_prices = elspot.Prices()
